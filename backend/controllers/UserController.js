258237f8b98a2dc45a51749da4472b6c370e4707
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-01-30 11:24:35 -0300

    create user (backend)

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -14,7 +14,37 @@ const generateToken = (id) => {
 
 // Register user and sign in
 const register = async (req, res) => {
-    res.send("Registro")
+    
+    const {name, email, password} = req.body
+
+    // check if user exists
+    const user = await User.findOne({email})
+
+    if (user) {
+        res.status(422).json({errors: ["The email address is already in use"]})
+        return
+    }
+
+    // Generate password hash
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // Create user
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    })
+
+    // If user was created successfully, return the token
+    if (!newUser) {
+        res.status(422).json({errors: ["An error ocurred. Please, try again later."]})
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser.id)
+    })
 }
 
 module.exports = {
---
+++ backend/middlewares/userValidations.js
@@ -0,0 +1,37 @@
+const { body } = require("express-validator")
+
+const userCreateValidation = () => {
+    return [
+        body("name")
+            .isString()
+            .withMessage("Name is required")
+            .isLength({ min: 3 })
+            .withMessage("The name has to be at least 3 characters long"),
+        
+        body("email")
+            .isString()
+            .withMessage("Email is required")
+            .isEmail()
+            .withMessage("Insert a valid email"),
+
+        body("password")
+            .isString()
+            .withMessage("Password is required")
+            .isLength({ min: 5 })
+            .withMessage("The name has to be at least 5 characters long"),
+
+        body("confirmpassword")
+            .isString()
+            .withMessage("Password confirmation is required")
+            .custom((value, { req }) => {
+                if (value != req.body.password) {
+                    throw new Error("Password confirmation must match")
+                }
+                return true;
+            })
+    ]
+}
+
+module.exports = {
+    userCreateValidation,
+}
