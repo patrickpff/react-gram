0caa9d1f089385afdf57c2a9435deeefeb49c848
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-04-23 17:36:24 -0300

    basic structure of register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,16 +8,22 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Login from './pages/Auth/Login'
 import Register from './pages/Auth/Register'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/login' element={<Login />}/>
-          <Route path='/register' element={<Register />}/>
-        </Routes>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/register' element={<Register />}/>
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </>
   )
--- frontend/src/pages/Auth/Register.tsx
+++ frontend/src/pages/Auth/Register.tsx
@@ -1,8 +1,32 @@
 import './Auth.css'
 
+// components
+import { Link } from 'react-router-dom'
+
+// hooks
+import { useState, useEffect } from 'react'
+
 const Register = () => {
+
+  const handleSubmit = () => {
+
+  }
+
   return (
-    <div>Register</div>
+    <div>
+      <h2>ReactGram</h2>
+      <p className='subtitle'>Sign-up to share photos with your friends!</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Name' />
+        <input type="email" placeholder='E-mail' />
+        <input type="password" placeholder='Password' />
+        <input type="password" placeholder='Confirm your password' />
+        <input type="submit" value="Register" />
+      </form>
+      <p>
+        Already has an account? <Link to="/login">Click here!</Link>
+      </p>
+    </div>
   )
 }
 
