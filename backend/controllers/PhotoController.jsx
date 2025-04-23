3bcc9a7a3e10b547b1bffbb6f633d51b4ab5a84a
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-04-23 11:07:30 -0300

    last functions of backend created and creation of frontend project

--- backend/controllers/PhotoController.jsx
+++ backend/controllers/PhotoController.jsx
@@ -202,6 +202,15 @@ const commentPhoto = async (req, res) => {
     })
 }
 
+// search photos by title
+const searchPhotos = async (req, res) => {
+    const {q} = req.query
+
+    const photos = await Photo.find({title: new RegExp(q, "i")}).exec()
+
+    res.status(200).json(photos)
+}
+
 module.exports = {
     insertPhoto,
     deletePhoto,
@@ -211,4 +220,5 @@ module.exports = {
     updatePhoto,
     likePhoto,
     commentPhoto,
+    searchPhotos,
 }
--- backend/routes/PhotoRoutes.jsx
+++ backend/routes/PhotoRoutes.jsx
@@ -11,6 +11,7 @@ const {
     updatePhoto,
     likePhoto,
     commentPhoto,
+    searchPhotos,
 } = require("../controllers/PhotoController.jsx")
 
 // Middlewares
@@ -24,6 +25,7 @@ router.post("/", authGuard, imageUpload.single("image"), photoInsertValidation()
 router.delete("/:id", authGuard, deletePhoto)
 router.get("/", authGuard, getAllPhotos)
 router.get("/user/:id", authGuard, getUserPhotos)
+router.get("/search", authGuard, searchPhotos)
 router.get("/:id", authGuard, getPhotoById)
 router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto)
 router.put("/like/:id", authGuard, likePhoto)
