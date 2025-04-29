655de7c63715cebee0eaf6e028228977f8581f2e
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-04-29 00:22:20 -0300

    login

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,8 +1,55 @@
 import './Auth.css'
 
+// Components
+import { Link } from 'react-router-dom'
+import Message from '../../components/Message'
+
+// Hooks
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+// Redux
+import { login, reset } from '../../slices/authSlice'
+
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const dispatch = useDispatch()
+
+  const { loading, error } = useSelector((state) => state.auth)
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const user = {
+      email,
+      password
+    }
+  
+    dispatch(login(user))
+  }
+
+  // Clean all auth states
+  useEffect(() => {
+    dispatch(reset())
+  }, [dispatch])
+   
   return (
-    <div>Auth</div>
+    <div id='login'>
+      <h2>ReactGram</h2>
+      <p className='subtitle'>Login to see what's new!</p>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='E-mail' onChange={(e) => setEmail(e.target.value)} value={email || ""}/>
+        <input type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)}  value={password || ""}/>
+        {!loading && <input type="submit" value="Login" />}
+        {loading && <input type="submit" value="Wait..." disabled />}
+        {error && <Message msg={error} type="error" />}
+      </form>
+
+      <p>Don't have an account? <Link to="/register">Click here!</Link></p>
+    </div>
   )
 }
 
--- frontend/src/slices/authSlice.jsx
+++ frontend/src/slices/authSlice.jsx
@@ -25,6 +25,21 @@ export const register = createAsyncThunk(
     }
 )
 
+// Login an user
+export const login = createAsyncThunk(
+    "auth/login", 
+    async (user, thunkAPI) => {
+        const data = await authService.login(user)
+
+        // check for errors
+        if (data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data
+    }
+)
+
 export const authSlice = createSlice({
     name: "auth",
     initialState,
@@ -36,25 +51,40 @@ export const authSlice = createSlice({
         }
     },
     extraReducers: (builder) => {
-        builder.addCase(register.pending, (state) => {
-            state.loading = true
-            state.error = false
-        }).addCase(register.fulfilled, (state, action) => {
-            state.loading = false
-            state.success = true
-            state.error = null
-            state.user = action.payload
-        })
-        .addCase(register.rejected, (state, action) => {
-            state.loading = false
-            state.error = action.payload
-            state.user = null
-        }).addCase(logout.fulfilled, (state, action) => {
-            state.loading = false
-            state.success = true
-            state.error = null
-            state.user = null
-        })
+        builder
+            .addCase(register.pending, (state) => {
+                state.loading = true
+                state.error = false
+            }).addCase(register.fulfilled, (state, action) => {
+                state.loading = false
+                state.success = true
+                state.error = null
+                state.user = action.payload
+            })
+            .addCase(register.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.payload
+                state.user = null
+            }).addCase(logout.fulfilled, (state, action) => {
+                state.loading = false
+                state.success = true
+                state.error = null
+                state.user = null
+            })
+            .addCase(login.pending, (state) => {
+                state.loading = true
+                state.error = false
+            }).addCase(login.fulfilled, (state, action) => {
+                state.loading = false
+                state.success = true
+                state.error = null
+                state.user = action.payload
+            })
+            .addCase(login.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.payload
+                state.user = null
+            })
     }
 })
 
